4d6a18d2588edeb9fc7b1330fc7acf6074602b1b
Juzer Tarwala <jtarwala@gmail.com>
2016-08-01 08:54:46 +0530

    nokia 1100 lcd project
    hey guyz this is a simple project of using your old nokia 1100 mobile lcd and display your name on it.I have provided the code which i tried and you can modified according to your need.I have provided all the file related to nokia lcd so you don't need to go anywhere else.I you like my work please to click the like button.

---
+++ nokia lcd/nokia_1100_lcd_code/nokia_1100_lcd_code.ino
@@ -0,0 +1,25 @@
+#include <PCF8814.h>
+
+static const byte ledPin = 13;
+static const byte LCD_WIDTH = 96;
+static const byte LCD_HEIGHT = 65;
+static PCF8814 lcd;
+
+void setup() {
+  lcd.begin(LCD_HEIGHT, LCD_WIDTH);
+  pinMode(ledPin, OUTPUT);
+  lcd.print("The LCD Totally Works!");
+  delay(5000);
+  lcd.clear();
+}
+
+
+void loop() {
+  lcd.setCursor(0, 0);
+  lcd.print("THIS IS JUZER");
+  lcd.setCursor(40, 1);
+  lcd.print("HELLO");
+  lcd.setCursor(0, 2);
+  lcd.print("FRIENDS");
+  delay(5000);
+}
